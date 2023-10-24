c111bcb1f26f7736f6c871f9b3c3377ed3649fcc
武迪 <wudi07@qianxin.com>
2023-10-24 17:30:14 +0800

    update code

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 #[no_mangle]
 pub extern "C" fn hello_world() {
-    let locale = fs::read("./test.txt").unwrap();
+    let test = fs::read("./test.txt").unwrap();
 
-    println!("Hello, {}", String::from_utf8(locale).unwrap());
+    println!("Hello, {}", String::from_utf8(test).unwrap());
 }
